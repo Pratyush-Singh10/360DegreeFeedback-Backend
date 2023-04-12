787418afb56a49f3cabc345f1dc5a31b4cca7831
Pratyush <pratyush.singh@accolitedigital.com>
2023-04-12 20:36:51 +0530

    Adding Project Starter

---
+++ src/test/java/com/example/FeedBackProject/FeedBackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FeedBackProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedBackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
