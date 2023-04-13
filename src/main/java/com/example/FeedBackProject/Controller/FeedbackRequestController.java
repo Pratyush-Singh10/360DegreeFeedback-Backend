788f6aeda53940a62bc207502ba8c0f800549fd7
Nettem Lakshmi <nettem.lakshmi@accolite.com>
2023-04-13 18:05:44 +0530

    adding feedbackrequest

--- src/main/java/com/example/FeedBackProject/Controller/FeedbackRequestController.java
+++ src/main/java/com/example/FeedBackProject/Controller/FeedbackRequestController.java
@@ -1,4 +1,27 @@
 package com.example.FeedBackProject.Controller;
 
+import com.example.FeedBackProject.Entity.FeedbackRequest;
+import com.example.FeedBackProject.Services.FeedbackRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
 public class FeedbackRequestController {
+    @Autowired
+    private FeedbackRequestService feedbackRequestService;
+
+    @PostMapping("/feedback-requests")
+    public ResponseEntity<FeedbackRequest> createFeedbackRequest(@RequestBody FeedbackRequest feedbackRequest) {
+        FeedbackRequest createdFeedbackRequest = feedbackRequestService.createFeedbackRequest(feedbackRequest);
+        return new ResponseEntity<>(createdFeedbackRequest, HttpStatus.CREATED);
+    }
+
 }
+
