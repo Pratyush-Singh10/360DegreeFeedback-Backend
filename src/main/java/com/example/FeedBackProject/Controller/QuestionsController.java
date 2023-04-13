0ca18cc95ef40763ccfceb5889e694a79a9f63ae
vignesh <vigneshkumar.s@accolitedigital.com>
2023-04-13 11:33:37 +0530

    changes

--- src/main/java/com/example/FeedBackProject/Controller/QuestionsController.java
+++ src/main/java/com/example/FeedBackProject/Controller/QuestionsController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/v1")
-public class QuestionsController {
-    @Autowired
-    private QuestionsService Qservice;
-
-    @GetMapping("/questions")
-    public
-}
+//@RestController
+//@RequestMapping("/api/v1")
+//public class QuestionsController {
+//    @Autowired
+//    private QuestionsService Qservice;
+//
+//    @GetMapping("/questions")
+//    public
+//}
