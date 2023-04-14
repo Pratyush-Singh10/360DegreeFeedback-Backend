191952c7d84d929673749071e8c440bf028c535a
Nettem Lakshmi <nettem.lakshmi@accolite.com>
2023-04-14 16:02:31 +0530

    changed schema

--- src/main/java/com/example/FeedBackProject/Controller/ResultController.java
+++ src/main/java/com/example/FeedBackProject/Controller/ResultController.java
@@ -1,4 +1,21 @@
 package com.example.FeedBackProject.Controller;
 
+import com.example.FeedBackProject.Entity.Result;
+import com.example.FeedBackProject.EntityDTO.ResultDTO;
+import com.example.FeedBackProject.Services.ResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ResultController {
+    @Autowired
+    private ResultService resultService;
+
+
 }
