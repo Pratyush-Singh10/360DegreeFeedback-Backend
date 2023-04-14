e6ca70d4167fe6d0692d252865d6ce62d085ac00
Navya R <navya.r@accolite.com>
2023-04-14 16:47:26 +0530

    hello

--- src/main/java/com/example/FeedBackProject/Repository/FeedbackRequestRepository.java
+++ src/main/java/com/example/FeedBackProject/Repository/FeedbackRequestRepository.java
@@ -16,21 +16,14 @@ public interface FeedbackRequestRepository extends JpaRepository<FeedbackRequest
     List<Object[]> findReceiverFeedbackDetails(@Param("email") String email);
 
 
-<<<<<<< HEAD
-    @Query(value = "select e.name,e.emp_id,fr.g_email,status from feedback_request fr, user e "+
-            "where fr.r_email=:email and fr.g_email=e.email_id;",nativeQuery = true)
-    List<Object[]> findPendingResponses(@Param("email") String email);
 
-    @Query(value = "select e.name,e.emp_id,fr.g_email from feedback_request fr, user e "+
-            "where fr.r_email=:email and status=1",nativeQuery = true)
-=======
     @Query(value = "select e.name,e.emp_id,fr.feedback_provider from feedback_request fr, user e "+
             "where fr.feedback_requester=:email and status=0 and fr.feedback_provider=e.email_id;",nativeQuery = true)
     List<Object[]> findPendingResponses(@Param("email") String email);
 
     @Query(value = "select e.name,e.emp_id,fr.feedback_provider from feedback_request fr, user e "+
             "where fr.feedback_requester=:email and status=1 and fr.feedback_provider=e.email_id;",nativeQuery = true)
->>>>>>> 191952c7d84d929673749071e8c440bf028c535a
+
     List<Object[]> findCompletedResponses(@Param("email") String email);
 
 
