188af0f20e24fe95aeedec0e1ed37cf2be751a1c
Pratyush <pratyush.singh@accolitedigital.com>
2023-04-24 17:48:39 +0530

    Resolved, API Call to get Avg of each Attribute for all requests

--- src/main/java/com/example/FeedBackProject/Repository/ResultRepository.java
+++ src/main/java/com/example/FeedBackProject/Repository/ResultRepository.java
@@ -17,10 +17,11 @@ public interface ResultRepository extends JpaRepository<Result, Long> {
     @Query(value = "select q.attribute,r.rating from result r, feedback_request fr, questions q where r.feedback_id=:id and r.feedback_id=fr.feedback_id and q.attribute_id=r.attribute_id ", nativeQuery = true)
     List<Object[]> findRating(@Param("id") Long id);
 
-    @Query(value = "select q.attribute_id, q.attribute, avg(res.rating) as AVG" +
-            " from questions q, result res, feedback_request fr" +
-            " where res.attribute_id=q.attribute_id and fr.feedback_requester = :email" +
-            " group by res.attribute_id",nativeQuery = true)
+    @Query(value = "select r.attribute_id, q.attribute, avg(r.rating) as AVG " +
+            "from questions q inner join result r on q.attribute_id = r.attribute_id " +
+            "inner join feedback_request f on f.feedback_id=r.feedback_id " +
+            "where f.feedback_requester= :email " +
+            "group by r.attribute_id, q.attribute ",nativeQuery = true)
     List<Object[]> findAvgRating(@Param("email") String email);
 
 }
