4d9ccaec46af08d7d78b177e3f9b091005e334b0
Nettem Lakshmi <nettem.lakshmi@accolite.com>
2023-04-24 15:48:31 +0530

    done with dynamic api call

--- src/main/java/com/example/FeedBackProject/Repository/ResultRepository.java
+++ src/main/java/com/example/FeedBackProject/Repository/ResultRepository.java
@@ -1,5 +1,6 @@
 package com.example.FeedBackProject.Repository;
 
+import com.example.FeedBackProject.Entity.FeedbackRequest;
 import com.example.FeedBackProject.Entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,7 @@ import java.util.List;
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long> {
 
-
+    List<Result> findByFeedbackId(FeedbackRequest feedbackRequest);
     @Query(value = "select q.attribute,r.rating from result r, feedback_request fr, questions q where r.feedback_id=:id and r.feedback_id=fr.feedback_id and q.attribute_id=r.attribute_id ", nativeQuery = true)
     List<Object[]> findRating(@Param("id") Long id);
 
--- src/main/java/com/example/FeedBackProject/Services/FeedbackRequestService.java
+++ src/main/java/com/example/FeedBackProject/Services/FeedbackRequestService.java
@@ -1,11 +1,28 @@
 package com.example.FeedBackProject.Services;
 
+import com.example.FeedBackProject.Entity.Questions;
+import com.example.FeedBackProject.Entity.Result;
+import com.example.FeedBackProject.Repository.ResultRepository;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+
 import com.example.FeedBackProject.Entity.FeedbackRequest;
 import com.example.FeedBackProject.Repository.FeedbackRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class FeedbackRequestService {
@@ -20,7 +37,6 @@ public class FeedbackRequestService {
         List<Object[]> employeeDetails = feedbackRequestRepository.findReceiverFeedbackDetails(email);
         return employeeDetails;
     }
-
 
 
     public List<Object[]> findPendingResponses(String email) {
@@ -42,13 +58,34 @@ public class FeedbackRequestService {
         String comment = feedbackRequestRepository.findComment(id);
         return comment;
     }
+
     public List<FeedbackRequest> getFeedbackRequestsByEmailAndStatus(String rEmail, int status) {
         return feedbackRequestRepository.findByrEmailAndStatus(rEmail, status);
     }
 
-    public List<Object[]> getResult(String rEmail) {
-        return feedbackRequestRepository.findResult(rEmail);
+
+    @Autowired
+    ResultRepository resultRepository;
+
+    public List<Map<String, Object>> getFeedbackByRequesterEmail(String email) {
+        List<Map<String, Object>> feedbackList = new ArrayList<>();
+        List<FeedbackRequest> feedbackRequests = feedbackRequestRepository.findByrEmail(email);
+        for (FeedbackRequest feedbackRequest : feedbackRequests) {
+            Map<String, Object> feedbackMap = new HashMap<>();
+            feedbackMap.put("feedbackId", feedbackRequest.getFeedbackId());
+            feedbackMap.put("projectName", feedbackRequest.getProjectName());
+            feedbackMap.put("feedbackProvider", feedbackRequest.getGEmail());
+            feedbackMap.put("selfInput", feedbackRequest.getSelfInput());
+            feedbackMap.put("startDate", feedbackRequest.getStartDate());
+            feedbackMap.put("endDate", feedbackRequest.getEndDate());
+            feedbackMap.put("feedbackComment", feedbackRequest.getFeedbackComment());
+            List<Result> results = resultRepository.findByFeedbackId(feedbackRequest);
+            for (Result result : results) {
+                Questions attribute = result.getAttributeId();
+                feedbackMap.put(attribute.getAttribute(), result.getRating());
+            }
+            feedbackList.add(feedbackMap);
+        }
+        return feedbackList;
     }
-
-
 }
