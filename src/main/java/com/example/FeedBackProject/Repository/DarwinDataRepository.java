dadb5c0b25aa6345ebc643ca85e84db1dce6377a
Pratyush <pratyush.singh@accolitedigital.com>
2023-05-05 10:24:27 +0530

    Removed Accolite Data

--- src/main/java/com/example/FeedBackProject/Repository/DarwinDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.FeedBackProject.Repository;
-
-import com.example.FeedBackProject.Entity.DarwinData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DarwinDataRepository extends JpaRepository<DarwinData, String > {
-}
